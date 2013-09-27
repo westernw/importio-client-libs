748cb7a3ad0539a96f5e6ad9cb48d6a75e8959d6
chrisalexander <chris.alexander@import.io>
2013-09-27 12:18:27 +0100

    Update PHP client to be wrapped up better

--- rest-php/rest.php
+++ rest-php/rest.php
@@ -1,22 +1,27 @@
 <?php
 
-$input = '{"input":{"???": "???"}}';
-$userGuid = "???";
-$apiKey = "???";
+// Put your User GUID here
+$userGuid = "";
+// Put your API key here
+$apiKey = "";
 
+function query($connectorGuid, $input, $userGuid, $apiKey) {
 
-$url = "https://api.import.io/store/connector/???/_query?_user=" . urlencode($userGuid) . "&_apikey=" . urlencode($apiKey);
+	$url = "https://query.import.io/store/connector/" . $connectorGuid . "/_query?_user=" . urlencode($userGuid) . "&_apikey=" . urlencode($apiKey);
 
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-curl_setopt($ch, CURLOPT_POSTFIELDS,  $input);
-curl_setopt($ch, CURLOPT_HEADER, 1);
-curl_setopt($ch, CURLOPT_POST, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	curl_setopt($ch, CURLOPT_POSTFIELDS,  json_encode(array("input" => $input)));
+	curl_setopt($ch, CURLOPT_POST, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
 
+	return json_decode($result);
+}
 
-echo 'Query Complete<br/><br/>';
-echo $result;
+// Example of doing a query
+$result = query("39df3fe4-c716-478b-9b80-bdbee43bfbde", array(
+	"input" => "query",
+), $userGuid, $apiKey);
 
-?>
+var_dump($result);
