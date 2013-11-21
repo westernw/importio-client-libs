0d0ba1fe2d016f8de09b808635df7a0e54034adf
Bam4d <chris.bamford@import.io>
2013-11-21 18:00:53 +0000

    PLATFORM-364 better management of poll threads

--- python/importio.py
+++ python/importio.py
@@ -126,6 +126,9 @@ class ImportIO:
         self.clientId = handshake.json[0]["clientId"]
     
     def connect(self):
+        if self.isConnected:
+            return;
+
         self.handshake()
         
         self.request("/meta/subscribe", data={"subscription":self.messagingChannel})
