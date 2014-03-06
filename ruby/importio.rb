54f695a0e673f940a75db012f8a98f31886613c1
chrisalexander <chris.alexander@import.io>
2014-03-06 12:15:51 +0000

    PLATFORM-392 Comments and example improvements in the Ruby client

--- ruby/importio.rb
+++ ruby/importio.rb
@@ -1,11 +1,13 @@
-'''
-
-import.io client library
-
-Dependencies=> Ruby 1.9, http-cookie
-
-@author=> import.io
-'''
+#
+# import.io client library - client classes
+# 
+# This file contains the main classes required to connect to and query import.io APIs
+#
+# Dependencies: Ruby 1.9, http-cookie
+#
+# @author: dev@import.io
+# @source: https://github.com/import-io/importio-client-libs/tree/master/python
+# 
 
 require "net/http"
 require "uri"
@@ -16,75 +18,97 @@ require "json"
 require "securerandom"
 
 class Query
+  # This class represents a single query to the import.io platform
   
   def initialize(callback, query)
+    # Initialises the new query object with inputs and default state
     @query = query
-    @jobsSpawned = 0
-    @jobsStarted = 0
-    @jobsCompleted = 0
+    @jobs_spawned = 0
+    @jobs_started = 0
+    @jobs_completed = 0
     @_finished = false
     @_callback = callback
   end
   
-  def _onMessage(data)
-    
-    msgType = data["type"]
-    if msgType == "SPAWN"
-      @jobsSpawned+=1
-    elsif msgType == "INIT" or msgType == "START"
-      @jobsStarted+=1
-    elsif msgType == "STOP"
-      @jobsCompleted+=1
+  def _on_message(data)
+    # Method that is called when a new message is received
+    # 
+    # Check the type of the message to see what we are working with
+    msg_type = data["type"]
+    if msg_type == "SPAWN"
+      # A spawn message means that a new job is being initialised on the server
+      @jobs_spawned+=1
+    elsif msg_type == "INIT" or msg_type == "START"
+      # Init and start indicate that a page of work has been started on the server
+      @jobs_started+=1
+    elsif msg_type == "STOP"
+      # Stop indicates that a job has finished on the server
+      @jobs_completed+=1
     end
       
-    @_finished = (@jobsStarted == @jobsCompleted and @jobsSpawned + 1 == @jobsStarted and @jobsStarted > 0)
+    # Update the finished state
+    # The query is finished if we have started some jobs, we have finished as many as we started, and we have started as many as we have spawned
+    # There is a +1 on jobs_spawned because there is an initial spawn to cover initialising all of the jobs for the query
+    @_finished = (@jobs_started == @jobs_completed and @jobs_spawned + 1 == @jobs_started and @jobs_started > 0)
     
-    # if there is an error or the user is not authorised correctly then allow isFinished to return true by setting jobs to -1
-    if msgType == "ERROR" or msgType == "UNAUTH" or msgType == "CANCEL"
-      @_finished = True
+    # These error conditions mean the query has been terminated on the server
+    # It either errored on the import.io end, the user was not logged in, or the query was cancelled on the server
+    if msg_type == "ERROR" or msg_type == "UNAUTH" or msg_type == "CANCEL"
+      @_finished = true
     end
     
+    # Now we have processed the query state, we can return the data from the message back to listeners
     @_callback.call(self, data)
   end
     
   def finished
+    # Returns boolean - true if the query has been completed or terminated
     return @_finished
   end 
 end
 
-class ImportIO
+class Importio
+  # The main import.io client, used for managing the message channel and sending queries and receiving data
 
   def initialize(userId=nil, apiKey=nil, host="https://query.import.io")
+    # Initialises the client library with its configuration
     @host = host
-    @proxyHost = nil
-    @proxyPort = nil
-    @msgId = 1
-    @clientId = nil
+    @proxy_host = nil
+    @proxy_port = nil
+    @msg_id = 1
+    @client_id = nil
     @url = "#{host}/query/comet/"
-    @messagingChannel = "/messaging"
+    @messaging_channel = "/messaging"
     @queries = Hash.new
     @userId = userId
     @apiKey = apiKey
     @cj = HTTP::CookieJar.new
     @queue = Queue.new
     @connected = false
+    @disconnecting = false
+    # These variables serve to identify this client and its version to the server
+    @clientName = "import.io Ruby client"
+    @clientVersion = "2.0.0"
   end
   
   def proxy(host,port)
-    @proxyHost = host
-    @proxyPort = port
+    # If you want to configure HTTP proxies, use this method to do so
+    @proxy_host = host
+    @proxy_port = port
   end
   
-  def makeRequest(url, data)
+  def make_request(url, data)
+    # Helper method that generates a request object
     uri = URI(url)
     request = Net::HTTP::Post.new(uri.request_uri)
     request.body = data
-    http = Net::HTTP.new(uri.host, uri.port, @proxyHost, @proxyPort)
+    http = Net::HTTP.new(uri.host, uri.port, @proxy_host, @proxy_port)
     http.use_ssl = uri.scheme == "https"
     return uri, http, request
   end
   
-  def open(uri, http, request) 
+  def open(uri, http, request)
+    # Makes a network request
     response = http.request(request)
     cookies = response.get_fields("set-cookie")
     if cookies != nil
@@ -96,12 +120,14 @@ class ImportIO
   end
   
   def encode(dict)
+    # Encodes a dictionary to x-www-form format
     dict.map{|k,v| "#{CGI.escape(k)}=#{CGI.escape(v)}"}.join("&")
   end
   
   def login(username, password, host="https://api.import.io")
+    # If you want to use cookie-based authentication, this method will log you in with a username and password to get a session
     data = encode({'username' => username, 'password'=> password})
-    uri, http, req = makeRequest("#{host}/auth/login", data )
+    uri, http, req = make_request("#{host}/auth/login", data )
     r = open(uri, http, req)
 
     if r.code != "200"
@@ -110,53 +136,69 @@ class ImportIO
   end
   
   def request(channel, path="", data={}, throw=true)
-    
-    # add in the common values
+    # Helper method that makes a generic request on the messaging channel
+
+    # These are CometD configuration values that are common to all requests we need to send
     data["channel"] = channel
     data["connectionType"] = "long-polling"
-    data["id"] = @msgId
-    @msgId += 1
+
+    # We need to increment the message ID with each request that we send
+    data["id"] = @msg_id
+    @msg_id += 1
     
-    if @clientId != nil
-      data["clientId"] = @clientId
+    # If we have a client ID, then we need to send that (will be provided on handshake)
+    if @client_id != nil
+      data["clientId"] = @client_id
     end
       
+    # Build the URL that we are going to request
     url = "#{@url}#{path}"
     
+    # If the user has chosen API key authentication, we need to send the API key with each request
     if @apiKey != nil
       q = encode({ "_user" => @userId, "_apikey" => @apiKey })
       url = "#{url}?#{q}"
     end
     
+    # Build the request object we are going to use to initialise the request
     body = JSON.dump([data])
-    uri, http, request = makeRequest(url, body)
+    uri, http, request = make_request(url, body)
     request.content_type = "application/json;charset=UTF-8"
     request["Cookie"] = HTTP::Cookie.cookie_value(@cj.cookies(uri))
+    request["import-io-client"] = @clientName
+    request["import-io-client-version"] = @clientVersion
     
+    # Send the request itself
     response = open(uri, http, request)
     if response.code != "200" 
-      raise "Connect failed, status #{response.code}"
+      raise "Unable to connect to import.io, status #{response.code} for url #{url}"
     end
     
     response.body = JSON.parse(response.body)
     
+    # Iterate through each of the messages in the response content
     for msg in response.body do
-
+      # If the message is not successful, i.e. an import.io server error has occurred, decide what action to take
       if msg.has_key?("successful") and msg["successful"] != true 
-        msg = "Unsuccessful request=> %s", msg
-        if throw
-          raise msg
+        msg = "Unsuccessful request: #{msg}"
+        if !@disconnecting and @connected
+          if throw
+            raise msg
+          else
+            print msg
+          end
         else
-          print msg
+          next
         end
-        
-        next
+
       end
       
-      if msg["channel"] != @messagingChannel
+      # Ignore messages that come back on a CometD channel that we have not subscribed to
+      if msg["channel"] != @messaging_channel
         next
       end
 
+      # Now we have a valid message on the right channel, queue it up to be processed
       @queue.push(msg["data"])
     
     end
@@ -166,32 +208,62 @@ class ImportIO
   end
   
   def handshake
+    # This method uses the request helper to make a CometD handshake request to register the client on the server
     handshake = request("/meta/handshake", path="handshake", data={"version"=>"1.0","minimumVersion"=>"0.9","supportedConnectionTypes"=>["long-polling"],"advice"=>{"timeout"=>60000,"interval"=>0}})
-    @clientId = handshake.body[0]["clientId"]
+    # Set the Client ID from the handshake's response
+    @client_id = handshake.body[0]["clientId"]
   end
   
+  def subscribe(channel)
+    # This method uses the request helper to issue a CometD subscription request for this client on the server
+    return request("/meta/subscribe", "", {"subscription"=>channel})
+  end
+
   def connect
+    # Connect this client to the import.io server if not already connected
+    # Don't connect again if we're already connected
     if @connected
       return
     end
+
+    # Do the hanshake request to register the client on the server
     handshake
     
-    request("/meta/subscribe", "", {"subscription"=>@messagingChannel})
+    # Register this client with a subscription to our chosen message channel
+    subscribe(@messaging_channel)
 
+    # Now we are subscribed, we can set the client as connected
     @connected = true
 
+    # Ruby's HTTP requests are synchronous - so that user apps can run while we are waiting for long connections
+    # from the import.io server, we need to pass the long-polling connection off to a thread so it doesn't block
+    # anything else
     @threads = []
     @threads << Thread.new(self) { |io|
       io.poll
     }
+
+    # Similarly with the polling, we need to handle queued messages in a separate thread too
     @threads << Thread.new(self) { |io|
-      io.pollQueue
+      io.poll_queue
     }
   end
 
   def disconnect
-    request("/meta/disconnect");
+    # Call this method to ask the client library to disconnect from the import.io server
+    # It is best practice to disconnect when you are finished with querying, so as to clean
+    # up resources on both the client and server
+    
+    # Set the flag to notify handlers that we are disconnecting, i.e. open connect calls will fail
+    @disconnecting = true
+    # Set the connection status flag in the library to prevent any other requests going out
     @connected = false
+    # Make the disconnect request to the server
+    request("/meta/disconnect");
+    # Now we are disconnected we need to remove the client ID
+    @client_id = nil
+    # We are done disconnecting so reset the flag
+    @disconnecting = false
   end
 
   def stop
@@ -201,8 +273,10 @@ class ImportIO
   end
   
   def join
+    # This method joins the threads that are running together, so we can wait for them to be finished
     while @connected
       if @queries.length == 0
+        # When there are no more queries, stop all the threads
         stop
         return
       end
@@ -210,10 +284,15 @@ class ImportIO
     end
   end
 
-  def pollQueue
+  def poll_queue
+    # This method is called in a new thread to poll the queue of messages returned from the server
+    # and process them
+
+    # This while will mean the thread keeps going until the client library is disconnected
     while @connected
       begin
-        processMessage @queue.pop
+        # Attempt to process the last message on the queue
+        process_message @queue.pop
       rescue => exception
         puts exception.backtrace
       end
@@ -221,15 +300,23 @@ class ImportIO
   end
 
   def poll
+    # This method is called in a new thread to open long-polling HTTP connections to the import.io
+    # CometD server so that we can wait for any messages that the server needs to send to us
+    
+    # While loop means we keep making connections until manually disconnected
     while @connected
+      # Use the request helper to make the connect call to the CometD endpoint
       request("/meta/connect", "connect", {}, false)
     end
   end
     
-  def processMessage(data)
+  def process_message(data)
+    # This method is called by the queue poller to handle messages that are received from the import.io
+    # CometD server
     begin
-      reqId = data["requestId"]
-      query = @queries[reqId]
+      # First we need to look up which query object the message corresponds to, based on its request ID
+      request_id = data["requestId"]
+      query = @queries[request_id]
       
       if query == nil 
         puts "No open query #{query}:"
@@ -237,9 +324,12 @@ class ImportIO
         return
       end
       
-      query._onMessage(data)
+      # Call the message callback on the query object with the data
+      query._on_message(data)
+
+      # Clean up the query map if the query itself is finished
       if query.finished
-        @queries.delete(reqId)
+        @queries.delete(request_id)
       end
     rescue => exception
       puts exception.backtrace
@@ -247,8 +337,15 @@ class ImportIO
   end
     
   def query(query, callback)
+    # This method takes an import.io Query object and issues it to the server, calling the callback
+    # whenever a relevant message is received
+    
+    # Set the request ID to a random GUID
+    # This allows us to track which messages correspond to which query
     query["requestId"] = SecureRandom.uuid
+    # Construct a new query state tracker and store it in our map of currently running queries
     @queries[query["requestId"]] = Query::new(callback, query)
+    # Issue the query to the server
     request("/service/query", "", { "data"=>query })
   end
 
