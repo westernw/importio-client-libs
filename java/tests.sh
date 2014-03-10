6dd21553e85f6bd06557ddc255cb913035c8d86d
chrisalexander <chris.alexander@import.io>
2014-03-10 13:02:50 +0000

    PLATFORM-392 Tests for the Java client

---
+++ java/tests.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Arguments: host, username, password, user GUID, API key
+pushd com.import.io.api.clientlite
+	mvn test -Dhost="$1" -Dusername="$2" -Dpassword="$3" -DuserGuid="$4" -DapiKey="$5"
+popd
